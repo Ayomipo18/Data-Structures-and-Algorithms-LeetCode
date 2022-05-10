2a7c1304846d3c30bf00577c1d173e30b0ed4d7c
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-10 23:29:42 +0100

    Time: 91 ms (61.16%), Space: 44.5 MB (89.70%) - LeetHub

---
+++ 46-permutations/46-permutations.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(letters) {
+    let res = [];
+    dfs(letters, [], Array(letters.length).fill(false), res);
+    return res;
+}
+
+function dfs(letters, path, used, res) {
+    if (path.length == letters.length) {
+        res.push(Array.from(path));
+        return;
+    }
+    for (let i = 0; i < letters.length; i++) {
+        if (used[i]) continue;
+        path.push(letters[i]);
+        used[i] = true;
+        dfs(letters, path, used, res);
+        path.pop();
+        used[i] = false;
+    }
+}
