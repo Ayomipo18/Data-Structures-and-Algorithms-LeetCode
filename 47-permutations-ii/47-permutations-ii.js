ca6e834fbdff284c119de9efc8d328f471f28145
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-13 12:25:00 +0100

    Time: 211 ms (23.04%), Space: 49 MB (27.86%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.js
+++ 47-permutations-ii/47-permutations-ii.js
@@ -2,34 +2,87 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permuteUnique = function(nums) {
-    const res = [];
-    const length = nums.length;
-    const visited = new Array(length).fill(0);
+var permuteUnique = function (nums) {
+    const result = [];
     const memo = new Set();
-    traverse(nums, res, [], length, visited, memo);
-    return res;
-};
+    const used = new Array(nums.length).fill(false);
+    traverse(nums, 0, [], result, used, memo);
+    return result;
+}
 
-const traverse = (nums, res, temp, length, visited, memo) => {
-    if (temp.length === length) {
-        res.push([...temp]);
+var traverse = function (nums, index, currArray, result, used, memo) {
+    if(currArray.length === nums.length) {
+        if(!memo.has(currArray.toString())) {
+           memo.add(currArray.toString());
+           result.push([...currArray]);
+           return;
+        }
         return;
-        
     }
     
-    for (let i = 0; i < length; i++) {
-        if (visited[i]) continue;
-        visited[i] = 1;
-        temp.push(nums[i]);
-        if (memo.has(temp.join(''))) {
-            temp.pop();
-            visited[i] = 0;
-            continue;
-        }
-        memo.add(temp.join(''));
-        traverse(nums, res, temp, length, visited, memo);
-        temp.pop();
-        visited[i] = 0;
+    for(let i=index; i<nums.length; i++) {
+        if(used[i]) continue;
+        currArray.push(nums[i]);
+        used[i] = true;
+        traverse(nums, index, currArray, result, used, memo);
+        currArray.pop();
+        used[i] = false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var permuteUnique = function(nums) {
+//     const res = [];
+//     const length = nums.length;
+//     const visited = new Array(length).fill(0);
+//     const memo = new Set();
+//     traverse(nums, res, [], length, visited, memo);
+//     return res;
+// };/
+
+// const traverse = (nums, res, temp, length, visited, memo) => {
+//     if (temp.length === length) {
+//         res.push([...temp]);
+//         return;
+        
+//     }
+    
+//     for (let i = 0; i < length; i++) {
+//         if (visited[i]) continue;
+//         visited[i] = 1;
+//         temp.push(nums[i]);
+//         if (memo.has(temp.join(''))) {
+//             temp.pop();
+//             visited[i] = 0;
+//             continue;
+//         }
+//         memo.add(temp.join(''));
+//         traverse(nums, res, temp, length, visited, memo);
+//         temp.pop();
+//         visited[i] = 0;
+//     }
+// }
