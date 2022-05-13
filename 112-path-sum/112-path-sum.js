010c44891f92d18df75d2296696b888657e59885
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-13 20:36:26 +0100

    Time: 89 ms (58.06%), Space: 45.5 MB (65.34%) - LeetHub

---
+++ 112-path-sum/112-path-sum.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root) return false;
+
+    if (!root.left && !root.right) {
+        return sum === root.val;
+    } else {
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+};
