6e1a8fd576146f1c444739217e57199ddb06cea5
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-13 12:26:33 +0100

    Time: 292 ms (13.88%), Space: 48.9 MB (30.03%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.js
+++ 47-permutations-ii/47-permutations-ii.js
@@ -29,37 +29,3 @@ var traverse = function (nums, index, currArray, result, used, memo) {
         used[i] = false;
     }
 }
-
-
-
-// var permuteUnique = function(nums) {
-//     const res = [];
-//     const length = nums.length;
-//     const visited = new Array(length).fill(0);
-//     const memo = new Set();
-//     traverse(nums, res, [], length, visited, memo);
-//     return res;
-// };/
-
-// const traverse = (nums, res, temp, length, visited, memo) => {
-//     if (temp.length === length) {
-//         res.push([...temp]);
-//         return;
-        
-//     }
-    
-//     for (let i = 0; i < length; i++) {
-//         if (visited[i]) continue;
-//         visited[i] = 1;
-//         temp.push(nums[i]);
-//         if (memo.has(temp.join(''))) {
-//             temp.pop();
-//             visited[i] = 0;
-//             continue;
-//         }
-//         memo.add(temp.join(''));
-//         traverse(nums, res, temp, length, visited, memo);
-//         temp.pop();
-//         visited[i] = 0;
-//     }
-// }
