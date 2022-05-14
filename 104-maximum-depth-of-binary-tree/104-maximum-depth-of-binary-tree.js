d101de556120d921f3edcd3a1017f22ddc0ce370
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-14 10:58:30 +0100

    Time: 79 ms (70.41%), Space: 45.3 MB (49.81%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const maxDepth = function(node) {
+    if(!node) return 0;
+    return Math.max(maxDepth(node.left) + 1, maxDepth(node.right) + 1)
+}
