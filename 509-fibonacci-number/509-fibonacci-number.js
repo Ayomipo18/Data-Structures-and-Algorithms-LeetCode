2a071aa7411a6d2e610260bf38709a73f4e198d8
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-20 23:35:09 +0100

    Time: 124 ms (19.42%), Space: 41.7 MB (74.75%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    const dp = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
