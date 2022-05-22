50ffa77de42eda422b01ed73068573b2ac299aca
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-22 08:34:44 +0100

    Time: 109 ms (19.87%), Space: 42.8 MB (75.40%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -9,30 +9,30 @@
 //   return cost[i] + Math.min(minCost(i - 1, cost), minCost(i - 2, cost));
 // }
 
-// const minCostClimbingStairs = function(cost) {
-//   const n = cost.length;
-//   if(n === 0) return 0;
-//   if(n === 1) return cost[0];
-//   for(let i = 0; i < n; i++) {
-//     if (i >= 2) {
-//       cost[i] = cost[i] + Math.min(cost[i - 1], cost[i - 2]);
-//     }
-//   }
-
-//   return Math.min(cost[n - 1], cost[n - 2]);
-// };
-
-var minCostClimbingStairs = function(cost) {
+const minCostClimbingStairs = function(cost) {
   const n = cost.length;
   if(n === 0) return 0;
   if(n === 1) return cost[0];
-  let costOne = cost[0];
-  let costTwo = cost[1];
-  for(let i = 2; i < n; i++) {
-    const current = cost[i] + Math.min(costOne, costTwo);
-    costOne = costTwo;
-    costTwo = current;
+  for(let i = 0; i < n; i++) {
+    if (i >= 2) {
+      cost[i] = cost[i] + Math.min(cost[i - 1], cost[i - 2]);
+    }
   }
 
-  return Math.min(costOne, costTwo);
+  return Math.min(cost[n - 1], cost[n - 2]);
 };
+
+// var minCostClimbingStairs = function(cost) {
+//   const n = cost.length;
+//   if(n === 0) return 0;
+//   if(n === 1) return cost[0];
+//   let costOne = cost[0];
+//   let costTwo = cost[1];
+//   for(let i = 2; i < n; i++) {
+//     const current = cost[i] + Math.min(costOne, costTwo);
+//     costOne = costTwo;
+//     costTwo = current;
+//   }
+
+//   return Math.min(costOne, costTwo);
+// };
