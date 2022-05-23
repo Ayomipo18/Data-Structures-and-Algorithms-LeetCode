63616843715d6a1acbfddb833c5a851be161d7e3
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-23 16:27:14 +0100

    Time: 278 ms (39.37%), Space: 49.2 MB (28.83%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -2,27 +2,33 @@
  * @param {string} s
  * @return {string}
  */
-// var longestPalindrome = function(s) {
-//     let max = '', str;
-//     for(let i=0; i<s.length; i++) {
-//         if(s.length === 1) return s;
-//         for(let j=i; j<s.length; j++) {
-//             str = s.substring(i, j+1);
-//            if(palindrome(str)) max = (str.length > max.length) ? str : max; 
-//         }
-//     }
-//     return max;
-// };
 
-// var palindrome = function(str) {
-//     let left = 0, right = str.length-1;
-//     while(left <= right) {
-//         if(str[left] != str[right]) return false;
-//         left++;
-//         right--;
-//     }
-//     return true;
-// }
+var longestPalindrome = function (s) {
+      let res = '';
+      for(let i = 0; i < s.length; i++){
+          // Odd - if there is only 1 char in the middle, like 'bob'
+          let lOdd = i, rOdd = i;
+          expandAroundCenter(lOdd, rOdd)
+          //Even - 2 chars in the middle, like 'kbbk'
+          let lEven = i, rEven = i + 1;
+          
+          
+          
+          expandAroundCenter(lEven, rEven);
+      }
+    // helper
+      function expandAroundCenter(left, right){
+        while(left >= 0 && right < s.length && s[left] === s[right]){
+          let subStr = s.substring(left, right + 1);
+          if(subStr.length > res.length){
+            res = subStr;
+          }
+          left--;
+          right++;
+        }
+      }
+      return res;
+};
 
 // var longestPalindrome = function (s) {
 //   for (let j = s.length - 1; j >= 0; j--) {
@@ -47,26 +53,25 @@
 //   return true;
 // }
 
-var longestPalindrome = function (s) {
-      let res = '';
-      for(let i = 0; i < s.length; i++){
-          // Odd - if there is only 1 char in the middle, like 'bob'
-          let lOdd = i, rOdd = i;
-          expandAroundCenter(lOdd, rOdd)
-          //Even - 2 chars in the middle, like 'kbbk'
-          let lEven = i, rEven = i + 1;
-          expandAroundCenter(lEven, rEven);
-      }
-    // helper
-      function expandAroundCenter(left, right){
-        while(left >= 0 && right < s.length && s[left] === s[right]){
-          let subStr = s.substring(left, right + 1);
-          if(subStr.length > res.length){
-            res = subStr;
-          }
-          left--;
-          right++;
-        }
-      }
-      return res;
-};
+// var longestPalindrome = function(s) {
+//     let max = '', str;
+//     for(let i=0; i<s.length; i++) {
+//         if(s.length === 1) return s;
+//         for(let j=i; j<s.length; j++) {
+//             str = s.substring(i, j+1);
+//            if(palindrome(str)) max = (str.length > max.length) ? str : max; 
+//         }
+//     }
+//     return max;
+// };
+
+// var palindrome = function(str) {
+//     let left = 0, right = str.length-1;
+//     while(left <= right) {
+//         if(str[left] != str[right]) return false;
+//         left++;
+//         right--;
+//     }
+//     return true;
+// }
+
