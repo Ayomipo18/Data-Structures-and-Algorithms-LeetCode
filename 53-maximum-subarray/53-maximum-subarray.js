76ede07e6826b038be56fdebe621422d9feab4d5
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-05-26 23:19:51 +0100

    Time: 125 ms (29.56%), Space: 50.1 MB (88.78%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -2,13 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var maxSubArray = function(nums) {
     let maxSub = nums[0];
-    let currentSum = 0;
-    for(i=0; i<nums.length; i++) {
-        if(currentSum < 0) currentSum = 0;
-        currentSum += nums[i];
-        maxSub = Math.max(maxSub, currentSum)
+    let currSum = 0;
+    for(let i=0; i < nums.length; i++) {
+        if(currSum < 0) currSum = 0;
+        currSum += nums[i];
+        maxSub = Math.max(currSum, maxSub);
     }
     return maxSub;
-};
+}
+
+
+
+// var maxSubArray = function(nums) {
+//     let maxSub = nums[0];
+//     let currentSum = 0;
+//     for(i=0; i<nums.length; i++) {
+//         if(currentSum < 0) currentSum = 0;
+//         currentSum += nums[i];
+//         maxSub = Math.max(maxSub, currentSum)
+//     }
+//     return maxSub;
+// };
