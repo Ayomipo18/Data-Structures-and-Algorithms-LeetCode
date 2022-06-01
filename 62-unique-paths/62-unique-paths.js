ed5f192f024160b051224a789c432c6b8ae07bdd
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-06-01 12:07:55 +0100

    Time: 88 ms (37.94%), Space: 42.3 MB (53.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -4,16 +4,11 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+    const dp = new Array(m).fill(0).map(() => new Array(n).fill(1));
     
-    for(let i = 0; i < m; i++) {
-        for(let j = 0; j < n; j++) {
-            if(i===0 && j===0) dp[i][j] = 1;
-            else {
-                if(i - 1 < 0) dp[i][j] = dp[i][j-1];
-                else if (j - 1 < 0) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
         }
     }
     return dp[m-1][n-1];
