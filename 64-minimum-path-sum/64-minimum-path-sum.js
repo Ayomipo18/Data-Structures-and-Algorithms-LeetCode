f47d120fe41a734f4b1bfed401b7a1e8c9ddaed0
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-06-05 17:47:12 +0100

    Time: 122 ms (23.78%), Space: 42.4 MB (97.62%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.js
+++ 64-minimum-path-sum/64-minimum-path-sum.js
@@ -5,7 +5,6 @@
 var minPathSum = function(grid) {
     const row = grid.length;
     const col = grid[0].length;
-    const dp = new Array(row).fill(0).map(() => new Array(col).fill(0));
     
     for(let i = 0; i<row; i++) {
         for(let j = 0; j<col; j++) {
