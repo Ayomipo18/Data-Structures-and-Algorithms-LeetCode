390a9403857aef3c72416eb171b8a6fbe4c74290
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-06-11 23:46:51 +0100

    Time: 173 ms (62.80%), Space: 41.4 MB (92.08%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cs
+++ 90-subsets-ii/90-subsets-ii.cs
@@ -2,21 +2,20 @@ public class Solution {
     public IList<IList<int>> SubsetsWithDup(int[] nums) {
         List<IList<int>> result = new List<IList<int>>();
         Array.Sort(nums);
-        BackTrack(0, new List<int>());
-        
-        void BackTrack(int index, List<int> currArr) {
-            result.Add(new List<int>(currArr));
-            if(index >= nums.Length) 
-                return;
-            for(int i=index; i<nums.Length; i++) {
-                if(i>index && nums[i-1] == nums[i])
-                    continue;
-                currArr.Add(nums[i]);
-                BackTrack(i+1, currArr);
-                currArr.Remove(nums[i]);
-            }
-        }
-        
+        BackTrack(0, new List<int>(), result, nums);
         return result;
+    }
+    
+    void BackTrack(int index, List<int> currArr, List<IList<int>> result, int[] nums) {
+        result.Add(new List<int>(currArr));
+        if(index >= nums.Length) 
+            return;
+        for(int i=index; i<nums.Length; i++) {
+            if(i>index && nums[i-1] == nums[i])
+                continue;
+            currArr.Add(nums[i]);
+            BackTrack(i+1, currArr, result, nums);
+            currArr.Remove(nums[i]);
+        }
     }
 }
