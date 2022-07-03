26f2e253b1c2f8cf57353c730896b1969706c3c7
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-07-03 16:43:48 +0100

    Time: 147 ms (18.33%), Space: 36.9 MB (23.33%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cs
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cs
@@ -2,22 +2,16 @@ public class Solution {
     public int WiggleMaxLength(int[] nums) {
         
         int count = 1;
-        int prevNumber;
         int prevDifference = 0;
         int difference;
         
         if(nums.Length == 1) return count;
         
         for(int i = 1; i < nums.Length; i++) {
-            prevNumber = nums[i-1];
-            if(nums[i] == prevNumber) continue;
-            else {
-                difference = nums[i] - prevNumber;
-                if((difference > 0 && prevDifference <= 0) ||(difference < 0 && prevDifference >= 0)) {
-                    count++;
-                    prevDifference = difference;
-                }
-                else continue;
+            difference = nums[i] - nums[i-1];
+            if((difference > 0 && prevDifference <= 0) ||(difference < 0 && prevDifference >= 0)) {
+                count++;
+                prevDifference = difference;
             }
         }
         
