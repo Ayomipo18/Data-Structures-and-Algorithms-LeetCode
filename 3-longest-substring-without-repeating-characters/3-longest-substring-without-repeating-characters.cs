01e22d06efd64977149c4e7ea033eec8a573f2ac
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-07-10 20:19:45 +0100

    Time: 255 ms (13.50%), Space: 37.1 MB (30.59%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cs
@@ -1,22 +1,26 @@
 public class Solution {
     public int LengthOfLongestSubstring(string s) {
-        List<char> letters = new List<char>();
-        int maxLength = 0;
+        if(s.Length == 0) return 0;
         
-        foreach (char c in s) {
-            if (!letters.Contains(c)) {
-                letters.Add(c);
-            }
-            else {
-                maxLength = Math.Max(maxLength, letters.Count);
-                
-                int idx = letters.IndexOf(c);
-                letters.RemoveRange(0, idx+1);
-
-                letters.Add(c);
-            }
+        int maxLength = 1;
+        
+        for(int i = 0; i < s.Length; i++) {
+            HashSet<char> hashset = new HashSet<char>();
+            hashset.Add(s[i]);
+            int currLength = 1;
+            currLength = dfs(i+1, hashset, s, currLength);
+            maxLength = Math.Max(maxLength, currLength);
         }
         
-        return Math.Max(maxLength, letters.Count); 
+        return maxLength;
+    }
+    
+    public int dfs(int index, HashSet<char> hashset, string s, int currLength) {
+        if(index == s.Length || hashset.Contains(s[index])) {
+            return currLength;
+        }
+        currLength++;
+        hashset.Add(s[index]);
+        return dfs(index+1, hashset, s, currLength);
     }
 }
