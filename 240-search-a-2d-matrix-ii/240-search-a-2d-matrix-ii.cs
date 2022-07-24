0f6ebe49270ece46a11d32a7139eb5a7463acc7c
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-07-24 14:13:36 +0100

    Time: 297 ms (30.95%), Space: 45.5 MB (53.35%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cs
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cs
@@ -1,14 +1,38 @@
+// public class Solution {
+//     public bool SearchMatrix(int[][] matrix, int target) {
+//         int row = 0;
+//         int col = matrix[0].Length-1;
+        
+//         while(col >= 0 && row < matrix.Length) {
+//             if(matrix[row][col] == target) return true;
+//             else if(matrix[row][col] > target) col--;
+//             else if(matrix[row][col] < target) row++;
+//         }
+        
+//         return false;
+        
+//     }
+// }
+
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
-        int row = 0;
-        int col = matrix[0].Length-1;
-        
-        while(col >= 0 && row < matrix.Length) {
-            if(matrix[row][col] == target) return true;
-            else if(matrix[row][col] > target) col--;
-            else if(matrix[row][col] < target) row++;
+        foreach(int[] arr in matrix) {
+            if(binarySearch(arr, target)) return true;
         }
         
         return false;
     }
+    
+    public bool binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.Length-1;
+        while(left <= right) {
+            int middle = (left + right)/2;
+            if(arr[middle] == target) return true;
+            else if(arr[middle] > target) right = middle-1;
+            else if(arr[middle] < target) left = middle+1;
+        }
+        return false;
+    }
+    
 }
