c28f98e4b617b4d8bf2a55059905d2936dc2bfb6
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-08-12 11:37:33 +0100

    Time: 153 ms (96.15%), Space: 44.8 MB (6.47%) - LeetHub

--- 1-two-sum/1-two-sum.cs
+++ 1-two-sum/1-two-sum.cs
@@ -1,12 +1,20 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        var dict = new Dictionary<int, int>();
-        for(int i = 0; i<nums.Length; i++) {
-            if(dict.ContainsKey(nums[i])) {
-                return new int[]{dict[nums[i]], i};
-            }
-            dict[target - nums[i]] = i;
+        Dictionary<int, int> dict = new Dictionary<int, int>(); // 10, 0 10, 1
+        for(int i = 0; i< nums.Length; i++){ // i 0 1
+            if(dict.ContainsKey(nums[i])) return new int[]{i, dict[nums[i]]};
+            if(dict.ContainsKey(target - nums[i])) continue;
+            dict.Add(target - nums[i], i);
         }
         return new int[2];
     }
 }
+
+//[1,2,3,4] t=6
+//output array
+//for(int i = 0; i< nums.Length; i++)
+//i = 0 nums[i] = 3
+//for(int j = i + 1; j < nums.Length; j++)
+//j = 1 nums[j] = 3
+//if(nums[i] + nums[j] == t) return new int[]{i,j};
+//return new int[2];
