1fa75f60f41f6bf457abd9f90c8a9b4f2911c74c
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-08-14 07:56:44 +0100

    Time: 215 ms (63.00%), Space: 40.7 MB (87.75%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
@@ -1,66 +1,58 @@
-public class Solution 
-{
-    public int ShortestPathBinaryMatrix(int[][] grid) 
-    {
-        int l = grid.Length;
+public class Solution {
+    public int ShortestPathBinaryMatrix(int[][] grid) {
+        if(grid == null || grid.Length == 0) return -1;
+        int n = grid.Length;
         
-        if (l == 1 && grid[0][0] == 0)
-        {
-            return 1;
-        }
-        if (grid[0][0] == 1 || grid[l - 1][l - 1] == 1)
-        {
-            return -1;
-        }
+        Queue<int[]> queue = new Queue<int[]>();
+        queue.Enqueue(new int[]{0,0});
         
-        grid[0][0] = 1;
-        int res = 1;
+        if(grid[0][0] == 1) return -1;
         
-        Queue<int[]> q = new();
-        q.Enqueue(new int[]{0, 0});
+        bool[,] visited = new bool[n,n];
+        visited[0,0] = true;
         
-        int[][] dirs = new int[8][]
-        {
-            new int[]{1, 0},
-            new int[]{-1, 0},
-            new int[]{0, 1},
-            new int[]{0, -1},
-            new int[]{-1, -1},
-            new int[]{-1, 1},
-            new int[]{1, -1},
-            new int[]{1, 1}
-        };
-        
-        while (q.Count != 0)
-        {
-            int size = q.Count;
-            res ++;
-            
-            for (int i = 0; i < size; i ++)
-            {
-                int[] curr = q.Dequeue();
-                
-                foreach (int[] dir in dirs)
-                {
-                    int x = curr[0] + dir[0];
-                    int y = curr[1] + dir[1];
-                    
-                    if (x < 0 || x >= l || y < 0 || y >= l || grid[x][y] == 1)
-                    {
-                        continue;
-                    }
-                    if (x == l - 1 && y == l - 1)
-                    {
-                        return res;
-                    }
-                    
-                    q.Enqueue(new int[]{x, y});
-                    
-                    grid[x][y] = 1;
+        int level = 0;
+        List<(int x, int y)> directions = new List<(int x, int y)>();
+        directions.Add((0,1));
+        directions.Add((1,1));
+        directions.Add((1,0));
+        directions.Add((1,-1));
+        directions.Add((0,-1));
+        directions.Add((-1,-1));
+        directions.Add((-1,0));
+        directions.Add((-1,1));
+        //{{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1},{-1,0},{-1,1}};
+        while(queue.Count > 0) {
+            int count = queue.Count;
+            level++;
+            for(int i = 0; i < count; i++) {
+                int[] curr = queue.Dequeue();
+                int currX = curr[0];
+                int currY = curr[1];
+                if(currX == n-1 && currY == n-1) return level;
+                foreach(var dir in directions) {
+                    int x = currX + dir.x;
+                    int y = currY + dir.y;
+                    if(x < 0 || x > n-1 || y < 0 || y > n-1 || visited[x,y] || grid[x][y] > 0) continue;
+                    queue.Enqueue(new int[]{x,y});
+                    visited[x,y] = true;
                 }
             }
         }
         
         return -1;
     }
+    //so count = 1;
+    //start 0,0
+    //how do I go? top, diag, diag, right, down, diag, left
+    //so bfs
+    //queue = [[0,1],[1,0]]
+    //while(queue.Count > 0)
+    //dequeue = [0,1]
+    //for(length of dequeue)
+    //if(dequeue[i] == 0) increase path count and change it to 1;
+    //if it's 1, skip it
+    //how can bfs help me find shortest path
+    //so basically, just check each internal array and in the first array, make sure that 0 starts from 0,0
+    //and the last array, make sure the last zero is on the last right
 }
