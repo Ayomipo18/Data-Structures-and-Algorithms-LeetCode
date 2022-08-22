7a2ef868061dd000470b13b02c331905ffac91ee
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-08-22 22:38:32 +0100

    Time: 92 ms (97.22%), Space: 44.2 MB (30.56%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int ShortestPathLength(int[][] graph) {
+        if(graph.Length == 1) return 0;
+        
+        int finalState = (1 << graph.Length) - 1;
+        
+        Queue<int[]> queue =  new Queue<int[]>();
+        
+        for(int i = 0; i < graph.Length; i++) {
+            queue.Enqueue(new int[] {i, 1 << i});
+        }
+        
+        int[,] visited = new int[graph.Length, finalState + 1];
+        
+        int shortestPath = 0;
+        
+        while(queue.Count > 0) {
+            int size = queue.Count;
+            shortestPath++;
+            for(int i = 0; i < size; i++) {
+                int[] head = queue.Dequeue();
+                int nodeId = head[0];
+                int visitedNodeBitState = head[1];
+                
+                foreach(int neighbour in graph[nodeId]) {
+                    int newVisitedNodeBitState = visitedNodeBitState | (1 << neighbour);
+                    if(visited[neighbour, newVisitedNodeBitState] == 1) continue;
+                    visited[neighbour, newVisitedNodeBitState] = 1;
+                    if(newVisitedNodeBitState == finalState) return shortestPath;
+                    queue.Enqueue(new int[]{neighbour, newVisitedNodeBitState});
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
