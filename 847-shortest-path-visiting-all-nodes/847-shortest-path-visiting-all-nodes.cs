e91e956860fda6838cb0192ac1f440f29095ff6c
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-08-22 23:13:33 +0100

    Time: 139 ms (77.78%), Space: 42.2 MB (36.11%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cs
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cs
@@ -8,22 +8,22 @@ public class Solution {
         
         for(int i = 0; i < graph.Length; i++) {
             queue.Enqueue(new int[] {i, 1 << i});
-        }
+        } // O(n) - time; O(n) - space where n is the number of nodes, which is also the graph length
         
-        int[,] visited = new int[graph.Length, finalState + 1];
+        int[,] visited = new int[graph.Length, finalState + 1]; // O(n^2) - space
         
         int shortestPath = 0;
         
         while(queue.Count > 0) {
-            int size = queue.Count;
-            shortestPath++;
-            for(int i = 0; i < size; i++) {
-                int[] head = queue.Dequeue();
+            int size = queue.Count; // 4
+            shortestPath++; // 1
+            for(int i = 0; i < size; i++) { //O(n + edges)
+                int[] head = queue.Dequeue(); // [[0, 1]]
                 int nodeId = head[0];
                 int visitedNodeBitState = head[1];
                 
                 foreach(int neighbour in graph[nodeId]) {
-                    int newVisitedNodeBitState = visitedNodeBitState | (1 << neighbour);
+                    int newVisitedNodeBitState = visitedNodeBitState | (1 << neighbour); //1 | 2^1 = 3
                     if(visited[neighbour, newVisitedNodeBitState] == 1) continue;
                     visited[neighbour, newVisitedNodeBitState] = 1;
                     if(newVisitedNodeBitState == finalState) return shortestPath;
