1c5bf89d9e62415c326686d030f3901848eb8f1d
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-03 19:32:09 +0100

    Time: 136 ms (69.81%), Space: 51.5 MB (9.43%) - LeetHub

---
+++ 694-number-of-distinct-islands/694-number-of-distinct-islands.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    private IList<int[]> directions = new List<int[]>();
+    
+    public int NumDistinctIslands(int[][] grid) {
+        int n = grid.Length;
+        int m = grid[0].Length;
+        bool[,] visited = new bool[n,m];
+        HashSet<string> hashset = new();
+        
+        directions.Add(new int[]{-1,0}); //up
+        directions.Add(new int[]{0,1}); //right
+        directions.Add(new int[]{1,0}); //down
+        directions.Add(new int[]{0,-1}); //left
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(visited[i,j] != true && grid[i][j] == 1) {
+                    StringBuilder sb = new StringBuilder();
+                    dfs(i, j, grid, visited, sb, i, j);
+                    
+                    string key = sb.ToString();
+                    hashset.Add(key);
+                }
+            }
+        }
+        
+        return hashset.Count;
+    }
+    
+    private void dfs(int row, int col, int[][] grid, bool[,] visited, StringBuilder sb, int row0, int col0) {
+        int n = grid.Length;
+        int m = grid[0].Length;
+        visited[row, col] = true;
+        string key = $".{row-row0}.{col-col0}";
+        sb.Append(key);
+        
+        foreach(var direction in directions) {
+            int nextRow = row + direction[0];
+            int nextCol = col + direction[1];
+            if(nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= m || visited[nextRow, nextCol] == true || grid[nextRow][nextCol] == 0){
+                continue;
+            }
+            dfs(nextRow, nextCol, grid, visited, sb, row0, col0);
+        }
+    }
+}
