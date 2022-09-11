036d8efbddde53ed828ec8b459104b5cfce90937
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-11 19:55:46 +0100

    Time: 153 ms (15.00%), Space: 36.6 MB (72.50%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cs
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cs
@@ -2,14 +2,16 @@ public class Solution {
     //time - O(n)
     //space - O(1)
     public int MinStartValue(int[] nums) {
-        int sum = 0;
-        int minValue = Int32.MaxValue;
+        int sum = 0; // 0 - (-3) -  (-1) - (-4) - 0 - 2
+        // 0 - 1 - 3
+        int minValue = 0; //278654535467 - (-3) - (-4)
+        //0
         
         for(int i = 0; i < nums.Length; i++) {
             sum += nums[i];
             minValue = Math.Min(sum, minValue);
         }
         
-        return minValue < 0 ? Math.Abs(minValue) + 1 : 1;
+        return -minValue + 1;
     }
 }
