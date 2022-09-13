8aa1f3f2e6a6fdec1f108bf32842ead6ec53bb22
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-13 19:30:01 +0100

    Time: 239 ms (58.06%), Space: 45.7 MB (43.04%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cs
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cs
@@ -1,38 +1,54 @@
+public class Solution {
+    //time - O(m+n)
+    //space - O(1)
+    public bool SearchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].Length - 1;
+        
+        while(row < matrix.Length && col >= 0) {
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        
+        return false;
+    }
+}
+
 // public class Solution {
+//time - O(mlogm)
+//space - O(1)
 //     public bool SearchMatrix(int[][] matrix, int target) {
-//         int row = 0;
-//         int col = matrix[0].Length-1;
-        
-//         while(col >= 0 && row < matrix.Length) {
-//             if(matrix[row][col] == target) return true;
-//             else if(matrix[row][col] > target) col--;
-//             else if(matrix[row][col] < target) row++;
+//         foreach(int[] arr in matrix) {
+//             if(binarySearch(arr, target)) {
+//                 return true;
+//             }
 //         }
-        
 //         return false;
-        
+//     }
+    
+//     private bool binarySearch(int[] arr, int target) {
+//         int start = 0;
+//         int end = arr.Length;
+//         while(start <= end) {
+//             int mid = (start + end) / 2;
+//             if(arr[mid] == target) return true;
+//             else if(arr[mid] < target) {
+//                 start = mid + 1;
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+//         return false;
 //     }
 // }
 
-public class Solution {
-    public bool SearchMatrix(int[][] matrix, int target) {
-        foreach(int[] arr in matrix) {
-            if(binarySearch(arr, target)) return true;
-        }
-        
-        return false;
-    }
-    
-    public bool binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.Length-1;
-        while(left <= right) {
-            int middle = (left + right)/2;
-            if(arr[middle] == target) return true;
-            else if(arr[middle] > target) right = middle-1;
-            else if(arr[middle] < target) left = middle+1;
-        }
-        return false;
-    }
-    
-}
+//1st solution, start from the last col and first row(number 15)
+//if the value at that cell is == target , return
+//else if(cell < target) row++
+//else if(cell > target) col--
+
+//2nd solution, do binary search on each row, if any returns true, rteurn true else return false
