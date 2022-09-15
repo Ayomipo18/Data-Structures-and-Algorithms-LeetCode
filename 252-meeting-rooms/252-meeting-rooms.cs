00c5c1d5325b53f4bde090fd1892fd061f050402
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-15 10:41:17 +0100

    Time: 200 ms (22.05%), Space: 41.2 MB (54.75%) - LeetHub

---
+++ 252-meeting-rooms/252-meeting-rooms.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool CanAttendMeetings(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+        for(int i = 1; i < intervals.Length; i++) {
+            if(intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+//solution 1
+//sort by starting time
+//then do a for loop through the intevals, start at index 1
+//if curr start time < prev end time, return false
