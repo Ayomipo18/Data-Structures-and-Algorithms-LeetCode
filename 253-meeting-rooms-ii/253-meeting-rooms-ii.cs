015f77bba6a411002cbe5155e944959649174d5f
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-15 12:08:29 +0100

    Time: 203 ms (9.60%), Space: 39.3 MB (80.52%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cs
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cs
@@ -3,11 +3,11 @@ public class Solution {
     //time - O(nlogn)
     //space - O(n)
     public int MinMeetingRooms(int[][] intervals) {
-        Array.Sort(intervals, (a,b) => a[0].CompareTo(b[0]));
+        Array.Sort(intervals, (a,b) => a[0].CompareTo(b[0])); //O(nlogn)
         PriorityQueue<int, int> pq = new();
         int count = 1;
         pq.Enqueue(intervals[0][1], intervals[0][1]);
-        for(int i = 1; i < intervals.Length; i++) {
+        for(int i = 1; i < intervals.Length; i++) { //O(n)
             int prevEndTime = pq.Dequeue();
             int currStartTime = intervals[i][0];
             int currEndTime = intervals[i][1];
@@ -22,19 +22,9 @@ public class Solution {
 }
 
 //so we are basically concerned with each end time of prev and starting time of current
-//create a min heap [8,12]
-//count = 1
-//push 4 into it
-//start for loop at 1
-    //prev = pq.dequeue //4
-    //      7             < 4
-    //if curr value start < prev count++ //2
-        //push prev into heap also
-    //push end time into heap
 
-//return count;
-
-//create a min heap [20,30]
+//create a min heap [[0,30], [5,10], [15,20]]
+//heap = [20,30]
 //count = 1
 //push 30 into it
 //start for loop at 1
