90708d38f6350fe938da0526aeb9da3ad9ba10e8
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-16 09:28:00 +0100

    Time: 522 ms (18.98%), Space: 66.5 MB (24.62%) - LeetHub

---
+++ 359-logger-rate-limiter/359-logger-rate-limiter.cs
@@ -0,0 +1,30 @@
+public class Logger {
+    
+    //time - O(1)
+    //space - O(n)
+    Dictionary<string, int> times;
+    
+    public Logger() {
+        times = new Dictionary<string, int>();
+    }
+    
+    public bool ShouldPrintMessage(int timestamp, string message) {
+        if(times.ContainsKey(message)) {
+            if(timestamp - times[message] < 10) {
+                return false;
+            } else {
+                times[message] = timestamp;
+                return true;
+            }
+        } else {
+            times.Add(message, timestamp);
+            return true;
+        }
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * bool param_1 = obj.ShouldPrintMessage(timestamp,message);
+ */
