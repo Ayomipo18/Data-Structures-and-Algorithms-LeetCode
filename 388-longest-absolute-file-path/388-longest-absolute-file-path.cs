df37a08c070767d31d75c40ac3c94e0e6c3796ee
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-21 19:43:51 +0100

    Time: 128 ms (7.89%), Space: 35.9 MB (21.05%) - LeetHub

---
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int LengthLongestPath(string input) {
+        Stack<int> stack = new();
+        stack.Push(0); //[0, 4, 12, 22]
+        int maxLength = 0;
+        
+        //["dir", "\tsubdir1", "\tsubdir2", "\t\tfile.ext"]
+        foreach(string s in input.Split("\n")) {
+            int level = s.LastIndexOf("\t") + 1; //0 1 1 2
+            while(level < stack.Count - 1) {
+                stack.Pop();
+            }
+            //0 + (3-0) + 1 = 4
+            //4 + (8-1) + 1 = 12
+            //4 + (8-1) + 1 = 12
+            //12 + (10-1) + 1 = 22
+            int len = stack.Peek() + (s.Length - level) + 1;
+            //Console.WriteLine(len);
+            stack.Push(len);
+            if(s.Contains(".")) {
+                maxLength = Math.Max(maxLength, len-1);
+            }
+        }
+        
+        return maxLength;
+    }
+}
