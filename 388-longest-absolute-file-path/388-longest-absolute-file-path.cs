bb4068fb1fbc569c4e0b675710e8ad09f388de9d
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-21 19:55:53 +0100

    Time: 109 ms (15.79%), Space: 35.8 MB (21.05%) - LeetHub

--- 388-longest-absolute-file-path/388-longest-absolute-file-path.cs
+++ 388-longest-absolute-file-path/388-longest-absolute-file-path.cs
@@ -1,4 +1,6 @@
 public class Solution {
+    //time - O(n * l)
+    //space - O(l) where n = number of dir&files or lines, l = max level of depth
     public int LengthLongestPath(string input) {
         Stack<int> stack = new();
         stack.Push(0); //[0, 4, 12, 22]
