39d777c0b0037fe4428b1c1ec08ed6eb28c5794b
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-24 21:33:38 +0100

    Time: 246 ms (36.70%), Space: 43.4 MB (48.73%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,42 +1,45 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
-        int left = 0;
-        int right = nums.Length - 1;
+        int[] result = new int[]{-1, -1};
+        
+        if(nums.Length == 0) return result;
+        
         int firstPos;
         int lastPos;
         
-        firstPos = BinarySearchLeft(nums, left, right, target);
-        lastPos = BinarySearchRight(nums, left, right, target);
+        firstPos = BinarySearch(nums, target, 'l');
         
-        int[] result = new int[]{firstPos, lastPos};
+        if(firstPos == -1) {
+            return result;
+        }
+        
+        lastPos = BinarySearch(nums, target, 'r');
+        
+        result[0] = firstPos;
+        result[1] = lastPos;
+        
         return result;
     }
     
-    public int BinarySearchLeft(int[] nums, int left, int right, int target) {
+    public int BinarySearch(int[] nums, int target, char type) {
+        int left = 0;
+        int right = nums.Length - 1;
         int index = -1;
+        
         while(left <= right) {
             int middle = (left + right)/2;
             if(nums[middle] == target) {
                 index = middle;
-                right = middle - 1;
+                if(type == 'l') {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
             }
             else if(nums[middle] < target) left = middle + 1;
             else right = middle - 1;
         }
-        return index;
-    }
-    
-    public int BinarySearchRight(int[] nums, int left, int right, int target) {
-        int index = -1;
-        while(left <= right) {
-            int middle = (left + right)/2;
-            if(nums[middle] == target) {
-                index = middle;
-                left = middle + 1;
-            }
-            else if(nums[middle] > target) right = middle - 1;
-            else left = middle + 1;
-        }
+        
         return index;
     }
 }
