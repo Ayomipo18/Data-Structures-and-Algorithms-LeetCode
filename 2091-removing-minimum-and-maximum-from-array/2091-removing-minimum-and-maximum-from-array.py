5534606c6019c2dd82077ce2b2fad2914fdf7649
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-09-28 06:38:59 +0100

    Time: 1285 ms (70.23%), Space: 28.5 MB (31.27%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.py
@@ -1,28 +1,15 @@
-# class Solution:
-#     def minimumDeletions(self, nums: List[int]) -> int:
-#         min_index = 0
-#         max_index = 0
-#         min_num_of_deletions = 0
-#         for i in range(1, len(nums)):
-#             if nums[min_index] < nums[i]:
-#                 min_index = i
-#             if nums[max_index] > nums[i]:
-#                 max_index = i
-        
-#         mid = (len(nums) - 1) // 2
-#         if max(min_index, max_index) <= mid:
-#             min_num_of_deletions = max(min_index, max_index) + 1
-            
-#         else:
-#             min_num_of_deletions = min(min_index + 1, len(nums) - min_index) + min(max_index + 1, len(nums) - max_index)
-        
-#         return min_num_of_deletions
-        
 class Solution:
+    #where n = length of nums
+    #time - O(n)
+    #space - O(1)
     def minimumDeletions(self, nums: List[int]) -> int:
-        a=nums.index(max(nums))
-        i=nums.index(min(nums))
-        if a>=i:
-            return(min(a+1,len(nums)-i,len(nums)-a+i+1))
-        else:
-            return(min(i+1,len(nums)-a,len(nums)-i+a+1))
+        min_index = 0
+        max_index = 0
+        length = len(nums)
+        for i in range(1, len(nums)):
+            if nums[min_index] < nums[i]:
+                min_index = i
+            if nums[max_index] > nums[i]:
+                max_index = i
+        
+        return min(max(min_index, max_index) + 1, max(length - min_index, length - max_index), min_index + 1 + length - max_index, max_index + 1 + length - min_index)
