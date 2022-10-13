930eb07a88e16f190d358622bf94a6dd35afeb44
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-10-13 15:24:41 +0100

    Time: 3044 ms (73.07%), Space: 54 MB (39.18%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        from queue import PriorityQueue
+        
+        pq = PriorityQueue()
+        
+        intervals.sort(key = lambda x:x[0])
+        
+        for i, interval in enumerate(intervals):
+            if not pq.empty() and pq.queue[0] < interval[0]:
+                pq.get()
+                pq.put(interval[1])
+            else: 
+                pq.put(interval[1])
+                
+        return pq.qsize()
+            
+        #[1, 3] [4,5] [6, 8]
+        #[[1,5],[5,10],[2,3],[5,10],[6,8]]
