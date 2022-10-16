2114a7e08060fe2372f9086865110f211bfa2bca
Ayomipo18 <47226631+Ayomipo18@users.noreply.github.com>
2022-10-16 23:58:43 +0100

    Time: 4127 ms (5.01%), Space: 31.5 MB (22.75%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.py
+++ 2420-find-all-good-indices/2420-find-all-good-indices.py
@@ -1,15 +1,22 @@
 class Solution:
-	def goodIndices(self, nums: List[int], k: int) -> List[int]:
-		if k == 1:
-			return list(range(1, len(nums) - 1))
-		res = []
-		count = 1
-		for i in range(1, len(nums) - k - 1):
-			if nums[i] <= nums[i - 1] and nums[i + k + 1] >= nums[i + k]:
-				count += 1
-				if count >= k:
-					res.append(i + 1)
-			else:
-				count = 1
-            
-		return res
+    def goodIndices(self, nums: List[int], k: int) -> List[int]:
+        length = len(nums)
+        result = []
+        left = [1] * length
+        right = [1] * length
+        
+        for i in range(1, length):
+            if nums[i] <= nums[i-1]:
+                left[i] += left[i-1]
+                
+        for i in range(length - 2, -1, -1):
+            if nums[i] <= nums[i+1]:
+                right[i] += right[i+1]
+        
+        print(left)
+        print(right)
+        for i in range(k, length-k):
+            if left[i-1] >= k and right[i+1] >= k:
+                result.append(i)
+        
+        return result
