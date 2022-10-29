6914cdb3727575d435e31db1c78c4941cfb75003
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2022-10-29 23:18:09 +0100

    Time: 4963 ms (5.00%), Space: 86.9 MB (5.44%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -4,10 +4,10 @@ class Solution:
         self.memo = {}
 
     def countSubstrings(self, s: str) -> int:
-        self.backtrack(s, 0, len(s)-1)
+        self.dfs(s, 0, len(s)-1)
         return self.count
 
-    def backtrack(self, s, start, end):
+    def dfs(self, s, start, end):
         key = str(start) + "-" + str(end)
 
         if key in self.memo:
@@ -17,12 +17,12 @@ class Solution:
             return 1
         
         if s[start] == s[end]:
-            self.memo[key] = self.backtrack(s, start + 1, end - 1)
+            self.memo[key] = self.dfs(s, start + 1, end - 1)
         else:
             self.memo[key] = 0
         
-        self.backtrack(s, start, end - 1)
-        self.backtrack(s, start + 1, end)
+        self.dfs(s, start, end - 1)
+        self.dfs(s, start + 1, end)
         
         self.count += self.memo[key]
 
