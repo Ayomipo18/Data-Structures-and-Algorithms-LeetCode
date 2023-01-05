ed477ef5a8ac5b92d3057483686135026b1034a6
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-01-05 23:27:23 +0100

    Time: 1042 ms (84.19%), Space: 28.2 MB (81.61%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,12 +1,7 @@
 class Solution:
-    '''
-    time - O(n)
-    space - O(n)
-    where n is the length of tasks
-    '''
     def minimumRounds(self, tasks: List[int]) -> int:
         dict = {}
-        rounds = 0
+        min_rounds = 0
         
         for task in tasks:
             if task not in dict:
@@ -17,10 +12,10 @@ class Solution:
             if val == 1:
                 return -1
             if val % 3 == 0:
-                rounds += (val // 3)
+                min_rounds += (val // 3)
             elif val % 3 == 1:
-                rounds += ((val - 4) // 3) + 2
+                min_rounds += ((val - 4) // 3) + 2
             elif val % 3 == 2:
-                rounds += (val // 3) + 1
-        return rounds
+                min_rounds += (val // 3) + 1
+        return min_rounds
         
