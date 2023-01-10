c64e38b34e9293b20a5f12c415ddd397ca5632dc
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-01-10 20:37:48 +0100

    Time: 53 ms (94.58%), Space: 14.3 MB (91.77%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,25 @@
+class DSU:
+    def __init__(self, size):
+        self.parents = [i for i in range(size)]
+        
+    def union(self, x, y):
+        pX, pY = self.find(x), self.find(y)
+        self.parents[pY] = pX
+        
+    def find(self, val):
+        if self.parents[val] == val:
+            return val
+        self.parents[val] = self.find(self.parents[val])
+        return self.parents[val]
+    
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        dsu = DSU(n+1)
+        
+        for edge in edges:
+            if dsu.find(edge[0]) == dsu.find(edge[1]):
+                return edge
+            dsu.union(edge[0], edge[1])
+            
+        return [0,0]
