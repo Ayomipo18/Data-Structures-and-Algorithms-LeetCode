88d55cb4d07979d51672d4a5ca633e3d464507e0
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-01-23 22:46:17 +0100

    Time: 97 ms (89.86%), Space: 17.7 MB (64.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,16 @@
 class Solution:
     '''
-    m is the length of strs array
-    n is the max length of each word
-    time - O(m*n)
-    space - O(m*n)
+    solution 1
+    - we can go through each str in strs
+    - we sort each word and put it in an hashmap
+    - then add the string to its corresponding hashmap key
+    - at the end return hashmap.values()
     '''
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         result = defaultdict(list)
         
         for str_val in strs:
-            count = [0] * 26
-            for s in str_val:
-                count[ord(s)-ord('a')] += 1
-            result[tuple(count)].append(str_val)
+            key = ''.join(sorted(str_val))
+            result[key].append(str_val)
             
         return result.values()
