ead7f412b412d2a96db8b57cd062c4087523fc6b
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-01-31 13:35:28 +0100

    Time: 1974 ms (80.33%), Space: 63.1 MB (32.50%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,27 +1,37 @@
 class Solution:
     '''
-    time - O(nlogn)
-    space - O(n)
+    - Initialy, the idea is
+    - start from time 1, if the task at the moment is at the time to process it and computer is idle, then i process it
+    - [[1,2],[2,4],[3,2],[4,1]]
+    - time - 1 -> 2 -> 3 -> 4 -> 5 -> 6
+    - [[1,2],[8,4],[9,2],[10,1]]
+    - time1 - 1 -> 8 -> 9 -> 10
+    - to_process = [2(2), 3(1)]
+    - CPU1 - 0(1-3) -> 1(8-12)
+    - CPU - 0(1-3) -> 2(3-5) -> 3(5-6) -> 1(6-10)
+    
+    [(2,0)]
     '''
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        n = len(tasks)
+        result, minHeap = [], []
         
-        for i in range(n):
-            tasks[i].append(i)
-        tasks.sort()
+        for i, task in enumerate(tasks):
+            task.append(i)
         
-        ans, min_heap, time, i = [], [], tasks[0][0], 0
+        tasks.sort(key = lambda x : x[0])
         
-        while i < n or min_heap:
-            while i < n and time >= tasks[i][0]:
-                heapq.heappush(min_heap, [tasks[i][1], tasks[i][2]])
+        i, time = 0, tasks[0][0]
+        
+        while minHeap or i < len(tasks):
+            while i < len(tasks) and time >= tasks[i][0]:
+                heapq.heappush(minHeap, (tasks[i][1], tasks[i][2]))
                 i += 1
-            
-            if not min_heap:
+                
+            if not minHeap:
                 time = tasks[i][0]
             else:
-                process_time, index = heapq.heappop(min_heap)
-                ans.append(index)
-                time += process_time
-
-        return ans
+                proc_time, index = heapq.heappop(minHeap)
+                result.append(index)
+                time += proc_time
+                
+        return result
