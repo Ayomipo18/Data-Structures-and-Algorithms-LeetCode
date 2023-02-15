6e5713a1cde7854ec2c858d01272b0a997e04acb
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-02-15 20:28:40 +0100

    Time: 1995 ms (75.46%), Space: 63.4 MB (25.76%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,39 +1,45 @@
 class Solution:
     '''
-    - Initialy, the idea is
-    - start from time 1, if the task at the moment is at the time to process it and computer is idle, then i process it
-    - [[1,2],[2,4],[3,2],[4,1]]
-    - time - 1 -> 2 -> 3 -> 4 -> 5 -> 6
-    - [[1,2],[8,4],[9,2],[10,1]]
-    - time1 - 1 -> 8 -> 9 -> 10
-    - to_process = [2(2), 3(1)]
-    - CPU1 - 0(1-3) -> 1(8-12)
-    - CPU - 0(1-3) -> 2(3-5) -> 3(5-6) -> 1(6-10)
-    [(2,0)]
-    
-    - time - O(nlogn)
-    - space - O(n)
+    - [[1,2,0], [2,4,1], [3,2,2], [4,1,3]]
+    - i = 0 -> 1 -> 2 -> 3
+    - time = 1 -> 3 -> 6 -> 7 -> 11
+    - pq = []
+    - CPU = [0, 2, 3, 1]
+    - create the task and add their indexes
+    - sort the tasks
+    - go thru the tasks and take the current i
+    - time = tasks[0][0]
+    - while tasks[0][0] >= time:
+        - append tasks that are available for processing to pqueue
+        - we are sorting by processingtime
+    - There are two things,
+        - if there's something in our queue, meaning there's something up for processing, so pick the one with min processing time
+        - else if there's nothing there, pick the next time and set it as the next time.
+        time - O(nlogn)
+        space - O(n)
+        where n is the length of tasks array
     '''
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        result, minHeap = [], []
+        res = []
+        minHeap = []
+    
+        for index, task in enumerate(tasks):
+            task.append(index)
+            
+        tasks.sort()
+        i = 0
+        time = tasks[0][0]
         
-        for i, task in enumerate(tasks):
-            task.append(i)
-        
-        tasks.sort(key = lambda x : x[0])
-        
-        i, time = 0, tasks[0][0]
-        
-        while minHeap or i < len(tasks):
+        while i < len(tasks) or minHeap:
             while i < len(tasks) and time >= tasks[i][0]:
-                heapq.heappush(minHeap, (tasks[i][1], tasks[i][2]))
+                heapq.heappush(minHeap, [tasks[i][1], tasks[i][2]])
                 i += 1
-                
-            if not minHeap:
-                time = tasks[i][0]
-            else:
+                    
+            if minHeap:
                 proc_time, index = heapq.heappop(minHeap)
-                result.append(index)
                 time += proc_time
+                res.append(index)
+            else:
+                time = tasks[i][0]
                 
-        return result
+        return res
