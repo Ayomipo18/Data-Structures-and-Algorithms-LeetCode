30be04be49ca58d640a99346dea0fc63bdf32638
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-08-28 09:32:24 +0100

    Time: 42 ms (80.66%), Space: 16.5 MB (10.80%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,6 +7,8 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         '''
         - go through both lists and compare each current nodes, which ever is higher, put in the result and move the node in the list
+        space - O(list1 + list2)
+        time - O(List1 + list2)
         '''
         
         dummy = ListNode()
