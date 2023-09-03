bcd000cfa6de7016d9fcb2f0ed725f84860f117e
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-09-03 01:56:19 +0100

    Time: 42 ms (69.19%), Space: 16.2 MB (92.11%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,6 +1,11 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        #choose an arbitrary string as ref, most ikely the first string
+        #go thru the abritrary string, then pick each of the letters in each word and compare
+        #if the comparism is valid, add to result, else return result
+        
         res = ""
+        
         for i in range(len(strs[0])):
             for s in strs:
                 if i == len(s) or s[i] != strs[0][i]:
