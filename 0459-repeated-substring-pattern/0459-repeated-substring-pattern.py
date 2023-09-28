c384e3720b1e7d168a5c5d18314f3da7b27db1af
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-09-28 11:16:36 +0100

    Time: 43 ms (78.43%), Space: 16.3 MB (93.04%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,12 +1,19 @@
 class Solution:
-    def repeatedSubstringPattern(self, str):
-
-        """
-        :type str: str
-        :rtype: bool
-        """
-        if not str:
-            return False
-            
-        ss = (str + str)[1:-1]
-        return ss.find(str) != -1
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        '''
+        - Get pattern
+        - use pattern to check string
+        '''
+        length = len(s)
+        
+        for i in range(length//2, 0, -1):
+            if length % i == 0:
+                repeats = length // i
+                sub_str = s[0:i]
+                new_str = sub_str * repeats
+                if new_str == s:
+                    return True
+                
+        return False
+                
+    
