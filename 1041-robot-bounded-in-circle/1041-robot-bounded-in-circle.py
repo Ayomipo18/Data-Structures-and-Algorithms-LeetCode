b793c0be8530a0b33bf3fd26f5b95aad3faf7486
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-09-28 14:26:49 +0100

    Time: 33 ms (87.15%), Space: 16.2 MB (64.14%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -1,49 +1,21 @@
-# class Solution:
-#     def isRobotBounded(self, instructions: str) -> bool:
-#         '''
-#         - Keep the coordinates of the starting point
-#         - Do the sequence and see if you end up at the initial coordinates
-#         - Case G, Case L and Case R
-#         '''
-        
-#         x, y = 0, 0
-#         direction = 0
-#         turned = False
-        
-#         for s in instructions:
-#             if s == 'G':
-#                 if direction == 0 or direction == 2:
-#                     y += 1
-#                 elif direction == 1 or direction == 3:
-#                     x += 1
-#             elif s == 'L':
-#                 direction -= 1
-#                 if direction < 0:
-#                     direction = 3
-#                 turned = True
-#             elif s == 'R':
-#                 direction += 1
-#                 if direction > 3:
-#                     direction = 0
-#                 turned = True
-                
-#             print("x",x)
-#             print("y",y)
-#             print("dir", direction)
-#         return True if (x == 0 or y == 0) and (turned == True) else False
-
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
-        direction = (0,1)
-        start = [0,0]
+        '''
+        - Keep the coordinates of the starting point
+        - Do the sequence and see if you end up at the initial coordinates
+        - Case G, Case L and Case R
+        '''
         
-        for x in instructions:
-            if x == 'G':
+        start = [0,0]
+        direction = (0,1)
+        
+        for s in instructions:
+            if s == 'G':
                 start[0] += direction[0]
                 start[1] += direction[1]
-            elif x == 'L':
+            elif s == 'L':
                 direction = (-direction[1], direction[0])
-            elif x == 'R':
+            elif s == 'R':
                 direction = (direction[1], -direction[0])
-        
+                
         return start == [0,0] or direction != (0,1)
