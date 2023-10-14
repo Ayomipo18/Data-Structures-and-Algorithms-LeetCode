99347a80c80ba0d7219db842b0d2bf0fee5527bc
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-10-14 23:58:42 +0100

    Time: 129 ms (89.56%), Space: 21 MB (89.56%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,32 @@
+# class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+#         result = []
+#         new_intervals = sorted(intervals, key=lambda x:x[0])
+#         result.append(new_intervals[0])
+        
+#         for i in range(1, len(new_intervals)):
+#             interval = new_intervals[i]
+#             if result[-1][1] < interval[0]:
+#                 result.append(interval)
+#             else:
+#                 result[-1][1] = interval[0]
+                
+#         return result
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
