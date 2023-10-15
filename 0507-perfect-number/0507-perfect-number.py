12a14349925f38a7b5b10ab8001c652adc1b92d2
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-10-16 00:42:30 +0100

    Time: 45 ms (49.62%), Space: 16.4 MB (12.91%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,27 +1,12 @@
-# class Solution:
-#     def checkPerfectNumber(self, num: int) -> bool:
-#         result = 1
-        
-#         for i in range(2, (num//2) + 1):
-#             if (num % i) == 0:
-#                 result += i
-                
-#         return result == num
-
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-
-        if num <= 1:
+        result = 1
+        
+        if num == 1:
             return False
-
-        limit = int(sqrt(num) + 1)
-        sum = 0
-        for i in range(1, limit):
-            if num % i == 0:
-                if num / i == i:
-                    sum += i
-                else:
-                    sum += i
-                    if i != 1:
-                        sum += num / i
-        return sum == num
+        
+        for i in range(2, int(math.sqrt(num)) + 1):
+            if (num % i) == 0:
+                result += i + num//i
+                
+        return result == num
