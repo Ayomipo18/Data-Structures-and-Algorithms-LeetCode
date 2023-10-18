e676a13998333d664b3c7e8a38c620c99c3609ca
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-10-18 09:45:39 +0100

    Time: 59 ms (87.88%), Space: 28.5 MB (76.86%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,46 +6,54 @@
 #         self.right = None
 
 class Solution:
-    p_set = []
-    q_set = []
+    # p_set = []
+    # q_set = []
         
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         '''
-        - at 5 - {3}
-        - at 1 - {3}
-        - at 6 - {5,3}
-        - at 2 - {5,3}
-        - at 0 - {1,3}
-        - at 8 - {1,3}
-        - at 7 - {3,5,2}
-        - at 4 - {3,5,2}
-        
-        time - O(logn) + O(logn*logn)
-        space - O(n)
         where n is the number of nodes
+        
+        solution 2
+        time - O(logn)
+        space - O(logn)
         '''
+        
+        if not root:
+            return None
+        
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        
+        else:
+            return left or right
     
-        tree_set = []
+#         tree_set = []
         
-        self.dfs(root, tree_set, p, q)
+#         self.dfs(root, tree_set, p, q)
         
-        for i in range(len(Solution.p_set)-1, -1, -1):
-            for j in range(len(Solution.q_set)-1, -1, -1):
-                if Solution.p_set[i] == Solution.q_set[j]:
-                    return Solution.p_set[i]
+#         for i in range(len(Solution.p_set)-1, -1, -1):
+#             for j in range(len(Solution.q_set)-1, -1, -1):
+#                 if Solution.p_set[i] == Solution.q_set[j]:
+#                     return Solution.p_set[i]
             
-        return 0
+#         return 0
     
-    def dfs(self, node, node_set, p, q):
-        node_set.append(node)
+#     def dfs(self, node, node_set, p, q):
+#         node_set.append(node)
         
-        if node.val == p.val:
-            Solution.p_set = node_set.copy() 
-        elif node.val == q.val:
-            Solution.q_set = node_set.copy()
+#         if node.val == p.val:
+#             Solution.p_set = node_set.copy() 
+#         elif node.val == q.val:
+#             Solution.q_set = node_set.copy()
         
-        if node.left:
-            self.dfs(node.left, node_set.copy(), p, q)
+#         if node.left:
+#             self.dfs(node.left, node_set.copy(), p, q)
             
-        if node.right:
-            self.dfs(node.right, node_set.copy(), p, q)
+#         if node.right:
+#             self.dfs(node.right, node_set.copy(), p, q)
