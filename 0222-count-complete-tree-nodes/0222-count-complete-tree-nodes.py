ed71c99089cdb573684827a06a4ce2a69d73d3c7
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-10-19 10:33:55 +0100

    Time: 73 ms (65.21%), Space: 23.7 MB (82.02%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -14,20 +14,5 @@ class Solution:
         
         if not root:
             return 0
-        left = root
-        right = root
-        l_h = 0
-        r_h = 0
-        
-        while left:
-            left = left.left
-            l_h += 1
-            
-        while right:
-            right = right.right
-            r_h += 1
-            
-        if l_h == r_h:
-            return int(math.pow(2, l_h)) - 1
         
         return 1 + self.countNodes(root.left) + self.countNodes(root.right)
