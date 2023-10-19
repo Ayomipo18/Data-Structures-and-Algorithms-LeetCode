e4d0880a11f2a1ff0a1228be0c3f4bacd509c374
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-10-19 10:58:06 +0100

    Time: 35 ms (86.27%), Space: 16.4 MB (22.72%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        '''
+        - use bfs
+        - priortize right, then left
+        - if a value for that particular level has been filled, don't replace it, else, replace int in the result array
+        where n is the number of nodes
+        time - O(n)
+        space - O(logn)
+        '''
+        result = []
+        if not root:
+            return result
+        
+        result.append(root.val)
+        
+        q = collections.deque()
+        q.append((root, 1))
+        
+        while q:
+            node, level = q.popleft()
+            
+            if node.right:
+                q.append((node.right, level+1))
+                if len(result) < level+1:
+                    result.append(node.right.val)
+                
+            if node.left:
+                q.append((node.left, level+1))
+                if len(result) < level+1:
+                    result.append(node.left.val)
+                    
+        return result
