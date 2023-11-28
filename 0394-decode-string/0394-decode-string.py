2110fac5705b59b1b9422bba35a6305f116e7d2f
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2023-11-28 17:37:08 +0100

    Time: 38 ms (52.41%), Space: 16.3 MB (47.48%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,27 +1,31 @@
 class Solution:
-    '''
-    time - O((maxk^countk)⋅n)
-    space - O(sum((maxk^countk)⋅n))
-    '''
     def decodeString(self, s: str) -> str:
-        from collections import deque
-        stack = deque()
-
+        '''
+        - have a stack to store your useful values (all letters of the alphabet, [, digits) 
+        - if i see an end bracket in my string, that's a cue to go to my stack and process the characters until i see [
+        - then gather all the digits after and multiply by the string processed.
+        - push the procesed string back into the stack.
+        '''
+        
+        stack = []
+        
         for val in s:
             if val == ']':
-                cur_str = ""
-                while stack[-1] != '[':
-                    val = stack.pop()
-                    cur_str = val + cur_str
-
-                stack.pop()
-                k = ""
-                while stack and stack[-1].isdigit():
-                    val = stack.pop()
-                    k = val + k
                 
-                stack.append(int(k) * cur_str)
+                cur_val = ''
+                while stack and stack[-1] != '[':
+                    cur_val = stack.pop() + cur_val
+                    
+                stack.pop()
+                
+                digit = ''
+                while stack and stack[-1].isdigit():
+                    digit = stack.pop() + digit
+                    
+                new_str = cur_val * int(digit)
+                stack.append(new_str)
+                
             else:
                 stack.append(val)
-
+            
         return "".join(stack)
