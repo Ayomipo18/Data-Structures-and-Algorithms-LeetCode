89dbec1e85a306af1abf8ca4ae64d7d56a90759a
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2024-04-03 22:35:44 +0100

    Time: 36 ms (99.80%), Space: 16.7 MB (9.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -6,27 +6,29 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         '''
-        - go through the length of the linkedlist
-        - add two numbers
-        - if there's a remainder, add it to carry else don't add it to carry
-        - there might a remainder and some values from the linkedlist, so basically try to add those remainders
+        - while we have l1.val or l2.val or carry > 0
+        - add l1, l2 and carry value
+        - If l1 value is null, make it zero
+        - if l2 value is null, make it zero
+        - Also, if l1.next doesn't exist, don't move
+        - Also, if l2.next doesn't exist, don't move
         '''
-        
         carry = 0
-        dummy = ListNode()
-        result = dummy
+        result = ListNode()
+        ans = result
         
         while l1 or l2 or carry > 0:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+            l1_val = 0 if l1 == None else l1.val
+            l2_val = 0 if l2 == None else l2.val
             
-            new_val = val1 + val2 + carry # 5+4 or 5+7
-            carry = new_val // 10
-            new_val %= 10
-            dummy.next = ListNode(new_val)
+            val = l1_val + l2_val + carry
+            
+            carry = int(val / 10)
+            val = int(val % 10)
             
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            dummy = dummy.next
+            result.next = ListNode(val)
+            result = result.next
             
-        return result.next
+        return ans.next
