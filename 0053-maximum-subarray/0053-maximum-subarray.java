51f7786b7b24c1b2a25ac3d33161e4c1f26bfc47
Ayomipo Solaja <47226631+Ayomipo18@users.noreply.github.com>
2026-07-29 20:37:40 +0100

    Time: 1 ms (99.95%), Space: 77.1 MB (66.54%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    /*
+    Time - O(n)
+    Space - O(1)
+    */
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int current_max_sum = nums[0];
+        int total_max_sum = nums[0];
+
+        for (int i=1; i<nums.length; i++) {
+            current_max_sum = Math.max(nums[i], nums[i]+current_max_sum);
+            total_max_sum = Math.max(total_max_sum, current_max_sum);
+        }
+
+        return total_max_sum;
+    }
+}
